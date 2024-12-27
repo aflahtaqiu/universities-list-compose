16e19192ff271ce153689f03a09962a9b921e910
aflahtaqiu <aflah.taiqu@mamiteam.com>
2024-12-27 18:39:23 +0700

    Feature: showing universities list - create the main viewmodel

---
+++ app/src/main/java/id/aflah/universitieslist/ui/screens/MainViewModel.kt
@@ -0,0 +1,30 @@
+package id.aflah.universitieslist.ui.screens
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import id.aflah.universitieslist.domain.ResultState
+import id.aflah.universitieslist.domain.entity.University
+import id.aflah.universitieslist.domain.usecase.GetUniversitiesListUseCase
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val getUniversitiesListUseCase: GetUniversitiesListUseCase,
+): ViewModel() {
+
+    val universitiesListState: MutableState<ResultState<List<University>>?> = mutableStateOf(null)
+
+    fun getUniversitiesList(country: String) {
+        viewModelScope.launch {
+            getUniversitiesListUseCase.execute(country).onEach {
+                universitiesListState.value = it
+            }.launchIn(viewModelScope)
+        }
+    }
+}
