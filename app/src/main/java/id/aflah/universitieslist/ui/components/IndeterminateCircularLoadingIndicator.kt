c3aeb5c1d7abb192d3838bf7db1c20713f91f504
aflahtaqiu <aflah.taiqu@mamiteam.com>
2024-12-28 10:57:36 +0700

    Feature: showing universities list - show loading animation - show snackbar when error fetch data

---
+++ app/src/main/java/id/aflah/universitieslist/ui/components/IndeterminateCircularLoadingIndicator.kt
@@ -0,0 +1,17 @@
+package id.aflah.universitieslist.ui.components
+
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun IndeterminateCircularIndicator(
+    modifier: Modifier = Modifier,
+) {
+    CircularProgressIndicator(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.secondary,
+        trackColor = MaterialTheme.colorScheme.surfaceVariant,
+    )
+}
--- app/src/main/java/id/aflah/universitieslist/ui/screens/MainScreen.kt
+++ app/src/main/java/id/aflah/universitieslist/ui/screens/MainScreen.kt
@@ -2,7 +2,9 @@ package id.aflah.universitieslist.ui.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -11,11 +13,15 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
@@ -23,24 +29,22 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import id.aflah.universitieslist.domain.ResultState
 import id.aflah.universitieslist.domain.entity.University
+import id.aflah.universitieslist.ui.components.IndeterminateCircularIndicator
 import id.aflah.universitieslist.ui.components.LinkText
 import id.aflah.universitieslist.ui.components.SearchBar
 import id.aflah.universitieslist.ui.theme.Typography
 import id.aflah.universitieslist.utils.cornerRadius
+import kotlinx.coroutines.launch
 
 @Composable
 fun MainScreen() {
     val mainViewModel = hiltViewModel<MainViewModel>()
     val shouldAppBarVisible = remember { mutableStateOf(false) }
-    var universitiesList = remember { mutableListOf<University>() }
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
 
     LaunchedEffect(Unit) {
         mainViewModel.getUniversitiesList("indonesia")
-    }
-
-    if (mainViewModel.universitiesListState.value is ResultState.Success<List<University>>) {
-        universitiesList =
-            (mainViewModel.universitiesListState.value as ResultState.Success<List<University>>).data as ArrayList
     }
 
     Scaffold(
@@ -57,6 +61,9 @@ fun MainScreen() {
                 )
             }
         },
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
         floatingActionButton = {
             FloatingActionButton(
                 modifier = Modifier.cornerRadius(32),
@@ -67,31 +74,55 @@ fun MainScreen() {
             ) {
                 Icon(Icons.Filled.Search, "", tint = Color.Black)
             }
-        }
+        },
     ) {
-        Box(Modifier.padding(it)) {
-            LazyColumn(
-                modifier = Modifier
-                    .padding(top = 8.dp, bottom = 8.dp, start = 9.dp, end = 9.dp)
-            ) {
-                items(universitiesList) { university ->
-                    Box(
-                        modifier = Modifier
-                            .padding(horizontal = 16.dp, vertical = 8.dp)
-                            .cornerRadius(8)
-                            .padding(horizontal = 8.dp)
+        Box(Modifier.padding(it).fillMaxSize()) {
+            when(mainViewModel.universitiesListState.value) {
+                is ResultState.Loading -> {
+                    IndeterminateCircularIndicator(
+                        modifier = Modifier.width(128.dp).align(Alignment.Center)
+                    )
+                }
+
+                is ResultState.Error -> {
+                    coroutineScope.launch {
+                        snackbarHostState
+                            .showSnackbar(
+                                message = "Failed feth data, please try again later!",
+                            )
+                    }
+                }
+
+                is ResultState.Success -> {
+                    LazyColumn(
+                        modifier = Modifier.padding(8.dp)
                     ) {
-                        Column {
-                            Text(university.name)
-                            Text(university.country)
-                            university.webPages.forEach { webPage ->
-                                LinkText(webPage)
+                        val universitiesList =
+                            (mainViewModel.universitiesListState.value as ResultState.Success<List<University>>).data as ArrayList
+                        items(universitiesList) { university ->
+                            Box(
+                                modifier = Modifier
+                                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                                    .cornerRadius(8)
+                                    .padding(horizontal = 8.dp)
+                            ) {
+                                Column {
+                                    Text(university.name)
+                                    Text(university.country)
+                                    university.webPages.forEach { webPage ->
+                                        LinkText(webPage)
+                                    }
+                                }
                             }
+                            HorizontalDivider(color = Color.Gray, thickness = 1.dp)
                         }
                     }
-                    HorizontalDivider(color = Color.Gray, thickness = 1.dp)
+                }
+
+                else -> {
+                    Box(Modifier.padding(it))
                 }
             }
         }
-        }
     }
+}
